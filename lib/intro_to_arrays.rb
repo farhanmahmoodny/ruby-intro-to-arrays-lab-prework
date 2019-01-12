fc33b6b3cf60eb242f944b3309e8266276b43d05
Farhan Mahmood <farhanmahmoodny@gmail.com>
2019-01-12 02:58:05 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -11,3 +11,14 @@ def first_element(array)
   array[0]
 end
 
+def third_element(array)
+  array[2]
+end
+
+def last_element(array)
+  array[-1]
+end
+
+def first_element_with_array_methods(array)
+  array.first
+end
